85e298bbe627342f0a93a56f32d2b7547b757831
Willy Mutesa <148581641+Mutesawilly@users.noreply.github.com>
2025-04-17 21:05:57 +0000

    Changes New

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'text-fill': {
+          '0%': { backgroundSize: '0% 100%' },
+          '100%': { backgroundSize: '100% 100%' },
+        },
+      },
+      animation: {
+        'text-fill': 'text-fill 2s forwards',
+      },
+    },
   },
+  variants: {
+    extend: {
+      animation: ['hover', 'focus'],
+    },
+  },  
   plugins: [],
 }
